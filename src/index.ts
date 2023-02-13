4d350ff08e9870b087587da88deeaaf0bfb2e9ed
jigaboogu <o.omarbekov77@gmail.com>
2023-02-13 16:55:10 +0600

    http only true

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ const main = async () => {
           }),
           cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-            httpOnly: false,
+            httpOnly: true,
             sameSite: "lax", // csrf
             secure: __prod__, // cookie only works in https
             domain: __prod__ ? ".onrender.com" : undefined,
